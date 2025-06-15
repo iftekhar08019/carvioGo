7ad6f97a17a6b52b4d8d5df48139882cf51a0efa
Md Iftekharul Alam <68458610+iftekhar08019@users.noreply.github.com>
2025-06-15 12:45:22 +0200

    recent car section added

---
+++ src/components/RecentList.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import CarCard from "./CarCard";
+import { Link } from "react-router";
+
+const RecentList = () => {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    fetch("/cars.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setCars(data);
+      })
+      .catch((err) => console.error("Failed to load recipes:", err));
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="lg:text-4xl text-xl font-bold text-center mt-15">
+        Recent Listed Cars
+      </h1>
+      <div className="lg:w-10/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {cars.map((car) => (
+          <CarCard key={car.id} car={car} />
+        ))}
+      </div>
+      <div className="card-actions mt-10">
+        <Link to="/all-cars" className="btn btn-primary">
+          View All Cars
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default RecentList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Feature from '../components/Features';
+import RecentList from '../components/RecentList';
 
 const Home = () => {
     return (
         <div>
             <Hero />
             <Feature />
+            <RecentList />
         </div>
     );
 };
